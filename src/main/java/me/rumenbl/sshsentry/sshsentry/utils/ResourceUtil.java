e4e4a4628ce7961912f2afb6969fc8635d009949
rumenbl <48157527+rumenbl@users.noreply.github.com>
2023-06-07 22:43:27 +0300

    Add: Implement method to parse a string out of a resource in classpath

--- src/main/java/me/rumenbl/sshsentry/sshsentry/utils/ResourceUtil.java
+++ src/main/java/me/rumenbl/sshsentry/sshsentry/utils/ResourceUtil.java
@@ -3,7 +3,23 @@ package me.rumenbl.sshsentry.sshsentry.utils;
 
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import org.springframework.core.io.Resource;
+import org.springframework.util.FileCopyUtils;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UncheckedIOException;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ResourceUtil {
+    public static String resourceToString(Resource resource) {
+        try (Reader reader = new InputStreamReader(resource.getInputStream(), UTF_8)) {
+            return FileCopyUtils.copyToString(reader);
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
 }
