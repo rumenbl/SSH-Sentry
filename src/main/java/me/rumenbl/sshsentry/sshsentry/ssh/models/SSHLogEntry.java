3b42228c7e8b53883762f2f648f6db0d2826b973
rumenbl <48157527+rumenbl@users.noreply.github.com>
2023-06-08 00:37:47 +0300

    Refactor: Remove bad constructor from SSHLogEntry

--- src/main/java/me/rumenbl/sshsentry/sshsentry/ssh/models/SSHLogEntry.java
+++ src/main/java/me/rumenbl/sshsentry/sshsentry/ssh/models/SSHLogEntry.java
@@ -2,12 +2,10 @@ package me.rumenbl.sshsentry.sshsentry.ssh.models;
 
 import lombok.Builder;
 import lombok.Data;
-import lombok.RequiredArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data
-@RequiredArgsConstructor
 @Builder
 public class SSHLogEntry {
     private String originIP;
