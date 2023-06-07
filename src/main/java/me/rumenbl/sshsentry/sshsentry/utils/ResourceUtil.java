b9a6748f9d75238b248ed4a8450b8ed3ea136b5e
rumenbl <48157527+rumenbl@users.noreply.github.com>
2023-06-07 22:42:43 +0300

    Add: Create ResourceUtil

---
+++ src/main/java/me/rumenbl/sshsentry/sshsentry/utils/ResourceUtil.java
@@ -0,0 +1,9 @@
+package me.rumenbl.sshsentry.sshsentry.utils;
+
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class ResourceUtil {
+}
