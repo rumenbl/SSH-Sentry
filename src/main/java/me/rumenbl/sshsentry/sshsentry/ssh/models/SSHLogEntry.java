9a7d28c6f18f73f970aa2ae54edd287d06496fa4
rumenbl <48157527+rumenbl@users.noreply.github.com>
2023-06-07 21:55:01 +0300

    Add: SSHLogEntry model properties

--- src/main/java/me/rumenbl/sshsentry/sshsentry/ssh/models/SSHLogEntry.java
+++ src/main/java/me/rumenbl/sshsentry/sshsentry/ssh/models/SSHLogEntry.java
@@ -1,4 +1,16 @@
 package me.rumenbl.sshsentry.sshsentry.ssh.models;
 
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@RequiredArgsConstructor
 public class SSHLogEntry {
+    private boolean isAccepted;
+    private String originIP;
+    private LocalDateTime timeOfEvent;
+    private String serverHostName;
+    private String serverIP;
 }
