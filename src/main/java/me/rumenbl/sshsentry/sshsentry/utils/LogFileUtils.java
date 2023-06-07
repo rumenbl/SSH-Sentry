e01c76f8e0058c16e78e425008b8dd955985a1bf
rumenbl <48157527+rumenbl@users.noreply.github.com>
2023-06-08 00:00:55 +0300

    Add: Implement logic for LogFileUtils

--- src/main/java/me/rumenbl/sshsentry/sshsentry/utils/LogFileUtils.java
+++ src/main/java/me/rumenbl/sshsentry/sshsentry/utils/LogFileUtils.java
@@ -1,9 +1,52 @@
 package me.rumenbl.sshsentry.sshsentry.utils;
 
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import me.rumenbl.sshsentry.sshsentry.ssh.models.SSHLogEntry;
 import org.springframework.stereotype.Component;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.LocalDateTime;
 
 @Component
 @Slf4j
 public class LogFileUtils {
+    private LogFileUtils() {
+    }
+
+    @SneakyThrows
+    public static SSHLogEntry parseSSHLogEntryFromString(final String str) {
+        final String[] arr = str.split(" ");
+        final String username = arr[9];
+        final String originIP = arr[11];
+        final String serverIP = pubIP();
+        final String serverHostname = arr[4];
+
+        return SSHLogEntry.builder()
+                .originIP(originIP)
+                .username(username)
+                .timeOfEvent(LocalDateTime.now())
+                .serverHostName(serverHostname)
+                .serverIP(serverIP).build();
+    }
+
+    private static String pubIP(){
+        String urlString = "https://checkip.amazonaws.com/";
+        URL url = null;
+        try {
+            url = new URL(urlString);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()))) {
+            return br.readLine();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
